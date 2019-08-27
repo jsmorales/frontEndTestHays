5759f59e83f7c4c3ece8bd6a7770a17b23da8014
jsmorales <johan.morales@saiavh.com>
2019-08-26 22:57:23 -0500

    Adds a new project for frontEnd test, this is the first commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {MainComponent} from './components/main/main.component';
+import {HomeComponent} from './components/home/home.component';
+import {StoreComponent} from './components/store/store.component';
+import {UserComponent} from './components/user/user.component';
+
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'store', component: StoreComponent},
+      {path: 'user', component: UserComponent},
+      {path: '', pathMatch: 'full', redirectTo: 'home'} // this path redirects to the child view employees on this case
+    ]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'main'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { SearchbarComponent } from './components/shaerd/searchbar/searchbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
+import { MainComponent } from './components/main/main.component';
+import { StoreComponent } from './components/store/store.component';
+import { FooterMenuComponent } from './components/shaerd/footer-menu/footer-menu.component';
+import { UserComponent } from './components/user/user.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    HomeComponent,
+    HomeHeaderComponent,
+    HomeContentComponent,
+    MainComponent,
+    StoreComponent,
+    FooterMenuComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
